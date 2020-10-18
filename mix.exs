7be3519fe1297ecd1d9332e376680dbf3c8e47c7
Olaf Hochherz <olaf@klingt.org>
2020-10-18 18:19:36 +0800

    remove unused dependancy

--- mix.exs
+++ mix.exs
@@ -27,8 +27,7 @@ defmodule ScExScsoundserver.MixProject do
     [
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:typed_struct, "~> 0.2.0"},
-      {:osc, "~> 0.1.2"},
-      {:sc_ex_synthdef, path: "../../github/sc_ex_synthdef/"}
+      {:osc, "~> 0.1.2"}
     ]
   end
 end
